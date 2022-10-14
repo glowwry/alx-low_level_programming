f6ffad21c870622f402ecf2da7d30412b9c844bd
glowwry <ebubenwankwo12@gmail.com>
2022-10-13 19:46:35 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - An entry point
+ * Return: Always give 0 if no error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
