c8bbea625133fda7afddc66361114e4f794b4cfe
glowwry <ebubenwankwo12@gmail.com>
2022-10-25 16:52:25 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaping integers
+ *
+ * @a- first integer
+ * @b- second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+	int d = *a;
+	*a = c;
+	*b = d;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Arrays are collections of variables of the same type, more like an element used to store a collection of data.
+All Arrays consists of contigous memory locations 
